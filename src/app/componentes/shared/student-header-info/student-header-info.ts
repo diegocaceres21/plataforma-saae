2c828bac0d8f37dba4451856889b7054bf159ea0
diegocaceres21 <diego.caceres@ucb.edu.bo>
2025-09-29 16:11:29 -0400

    Vista detallada por tipo de solicitud

---
+++ src/app/componentes/shared/student-header-info/student-header-info.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistroEstudiante } from '../../../interfaces/registro-estudiante';
+
+@Component({
+  selector: 'app-student-header-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student-header-info.html',
+  styleUrl: './student-header-info.scss'
+})
+export class StudentHeaderInfoComponent {
+  @Input() registro!: Partial<RegistroEstudiante>;
+  @Input() showManualOrderIndicator: boolean = false;
+  @Input() isInTieGroup: boolean = false;
+
+  isPlanPlus(): boolean {
+    return this.registro.plan_primer_pago?.toUpperCase() === 'PLAN PLUS';
+  }
+}
